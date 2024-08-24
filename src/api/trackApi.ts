d4616a4424c16f1d08cb70492f2e04c438c26864
dimych1978 <dimych1978@gmail.com>
2024-08-24 19:29:36 +0300

    call function reset at Centerblock.tsx

--- src/api/trackApi.ts
+++ src/api/trackApi.ts
@@ -1,4 +1,4 @@
-const URL = 'https://webev-music-003b5b991590.herokuapp.com/catalog/track/all';
+const URL = 'https://webdev-music-003b5b991590.herokuapp.com/catalog/track/all';
 export const getTracks = async () => {
   const response = await fetch(URL);
   if (!response.ok) {
