14fba3591ee330b1b84ec7610c894b52626f4e2a
dimych1978 <dimych1978@gmail.com>
2024-09-01 11:11:37 +0300

    realizing tracks playing and processing tracklist, inplementation RTK

--- src/components/PlayerBar/ProgressBar.tsx
+++ src/components/PlayerBar/ProgressBar.tsx
@@ -1,38 +1,27 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import styles from './ProgressBar.module.css';
+import { TimeType } from './PlayerBar';
 
 type props = {
-  widthBar: number;
-  backgroundBar: string;
-  setWidthBar: Dispatch<SetStateAction<number>>;
-  setBackgroundBar: () => void;
+  widthBar?: number;
+  backgroundBar?: string;
+  setWidthBar: React.Dispatch<React.SetStateAction<number>>;
+  setBackgroundBar: React.Dispatch<React.SetStateAction<string>>;
+  track?: HTMLAudioElement;
 };
+
+type timeProps = { time: TimeType };
 
 export const ProgressBar = ({
   widthBar,
   backgroundBar,
   setWidthBar,
   setBackgroundBar,
+  track,
 }: props) => {
-  // const [backgroundBar, setBackgroundBar] = useState<string>('2e2e2e');
-  // const [widthBar, setWidthBar] = useState<number>(0);
-
   const ref = useRef<HTMLDivElement | null>(null);
-
-  // useEffect(() => {
-  //   setWidthBar(0);
-  // }, [track]);
-
-  // update = () => {
-  //   console.log('object');
-  //   setWidthBar(
-  //     track!.duration ? (track!.currentTime / track!.duration) * 100 : 0
-  //   );
-  //   setBackgroundBar('#B672FF');
-  // };
-  console.log('widthBar', widthBar);
 
   const handleTime = (e: React.MouseEvent<HTMLDivElement>) => {
     const widthCurrent: number =
@@ -54,47 +43,13 @@ export const ProgressBar = ({
   );
 };
 
-// export const Time = ({ track, update }: props) => {
-//   type Time = {
-//     min: number;
-//     sec: number;
-//     minDuration: number;
-//     secDuration: number;
-//   };
-
-//   const [time, setTime] = useState<Time>({
-//     min: 0,
-//     sec: 0,
-//     minDuration: 0,
-//     secDuration: 0,
-//   });
-
-//   if (track) {
-//     track.oncanplay = () => {
-//       setTime({
-//         ...time,
-//         minDuration: track!.duration / 60,
-//         secDuration: track!.duration % 60,
-//       });
-//     };
-//   }
-
-//   if (track) {
-//     update = () => {
-//       setTime({
-//         ...time,
-//         min: track!.currentTime / 60,
-//         sec: track!.currentTime % 60,
-//       });
-//     };
-//   }
-
-//   return (
-//     <div className={styles.playerTime}>
-//       <div className={styles.time}>
-//         {time.min.toFixed()}мин. {time.sec.toFixed()}сек. из <br />
-//         {time.minDuration.toFixed()}.{time.secDuration.toFixed()}
-//       </div>
-//     </div>
-//   );
-// };
+export const TrackTime = ({ time }: timeProps) => {
+  return (
+    <div className={styles.playerTime}>
+      <div className={styles.time}>
+        {time.min.toFixed()} мин.{time.sec.toFixed()} сек. <br /> из <br />
+        {time.minDuration.toFixed()} мин.{time.secDuration.toFixed()} сек.
+      </div>
+    </div>
+  );
+};
