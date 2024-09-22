43c42327f5de37b41e3e44ce288c1ed7b08d32cb
dimych1978 <dimych1978@gmail.com>
2024-09-22 12:20:55 +0300

    продолжаю ошибаться

--- src/app/trackPages/base/page.tsx
+++ src/app/trackPages/base/page.tsx
@@ -1,19 +1,19 @@
-// 'use client';
+'use client';
 
-// import Centerblock from '@/components/Centerblock/Centerblock';
-// import PlayerBar from '@/components/PlayerBar/PlayerBar';
-// import { useAppSelector } from '@/store/store';
+import Centerblock from '@/components/Centerblock/Centerblock';
+import PlayerBar from '@/components/PlayerBar/PlayerBar';
+import { useAppSelector } from '@/store/store';
 
-// export default function Base() {
-//   const { thisTrack } = useAppSelector(state => state.tracksSlice);
+export default function Base() {
+  const { thisTrack } = useAppSelector(state => state.tracksSlice);
 
-//   const user = useAppSelector(state => state.auth);
-//   console.log('🚀 ~ Home ~ user:', user);
+  const user = useAppSelector(state => state.auth);
+  console.log('🚀 ~ Base ~ user:', user);
 
-//   return (
-//     <>
-//       <Centerblock />
-//       {thisTrack && <PlayerBar thisTrack={thisTrack} />}
-//     </>
-//   );
-// }
+  return (
+    <>
+      {/* <Centerblock /> */}
+      {/* {thisTrack && <PlayerBar thisTrack={thisTrack} />} */}
+    </>
+  );
+}
--- src/components/Centerblock/Centerblock.tsx
+++ src/components/Centerblock/Centerblock.tsx
@@ -2,15 +2,37 @@
 
 // import styles from './page.module.css';
 import { Nav } from '@/components/Nav/Nav';
-import Centerblock from '@/components/Centerblock/Centerblock';
+// import Centerblock from '@/components/Centerblock/Centerblock';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import PlayerBar from '@/components/PlayerBar/PlayerBar';
 import { useEffect, useState } from 'react';
 import { TrackType } from '@/types';
 import { useAppSelector } from '@/store/store';
+import { getTracks } from '@/api/trackApi';
+import ErrorPage from '@/app/error';
+import Content from '../Content/Content';
+import Filter from '../Filter/Filter';
 
-export default function Home() {
+export default function Centerblock() {
+  const [tracks, setTracks] = useState<TrackType[]>([]);
+
   const { thisTrack } = useAppSelector(state => state.tracksSlice);
+  useEffect(() => console.log('problem', thisTrack), []);
+  useEffect(() => {
+    const getData = async () => {
+      // let tracks: TrackType[] = [];
+      try {
+        const data = await getTracks();
+        setTracks(data);
+        console.log('tracks', data);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          return <ErrorPage error={error.message} reset={(() => {})()} />;
+        }
+      }
+    };
+    getData();
+  }, []);
 
   return (
     // <div className={styles.wrapper}>
@@ -18,9 +40,10 @@ export default function Home() {
     //     <main className={styles.main}>
     // <Nav />
     <>
-      <Centerblock />
       {/* // <Sidebar />
         // </main> */}
+      <Filter />
+      <Content />
       {thisTrack && <PlayerBar thisTrack={thisTrack} />}
     </>
     // /     <footer className='footer'></footer>
