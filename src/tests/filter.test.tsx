c62ad3e722f126f6df901a19f9eef227100a63cc
dimych1978 <dimych1978@gmail.com>
2024-10-12 07:50:36 +0300

    correcting name files and components at tests

--- src/tests/filter.test.tsx
+++ src/tests/filter.test.tsx
@@ -1,11 +1,11 @@
-import Content from '@/components/Content/Content';
+import Filter from '@/components/Filter/Filter';
 import ReduxProvider from '@/store/ReduxProvider';
 import { render } from '@testing-library/react';
 
 it('renders correctly', () => {
   const tree = render(
     <ReduxProvider>
-      <Content />
+      <Filter />
     </ReduxProvider>
   );
   expect(tree).toMatchSnapshot();
