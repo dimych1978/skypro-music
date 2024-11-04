50898247c425283118a7deccceacc628d03d2d36
dimych1978 <dimych1978@gmail.com>
2024-11-04 10:10:37 +0300

    watch select pages without change playlist

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,43 +1,17 @@
 'use client';
 
-import { useAppDispatch, useAppSelector } from '@/store/store';
+import { useAppSelector } from '@/store/store';
 import TrackItem from '../TrackItem/TrackItem';
-import { TrackType } from '@/types';
 import styles from '@/app/page.module.css';
-import { useGetTracks } from '@/hooks/useGetTracks';
-import { useEffect, useState } from 'react';
-import { setTrackState } from '@/store/features/trackSlice';
 
 const Select = () => {
-  const dispatch = useAppDispatch();
-  const { selectTitles, selectTracks } = useAppSelector(
+  const { selectTitles, tracks, selectArray } = useAppSelector(
     state => state.tracksSlice
   );
-
-  const [currentTracks, setCurrentTrack] = useState<TrackType[]>([]);
-
-  const { getAllTracks } = useGetTracks();
-
-  useEffect(() => {
-    try {
-      const getData = async () => {
-        const data = (await getAllTracks()) as TrackType[];
-        const filteredData = data.filter(track =>
-          selectTracks.includes(track._id)
-        );
-        dispatch(setTrackState(filteredData));
-        setCurrentTrack(filteredData);
-      };
-      getData();
-    } catch (error) {
-      console.log(error);
-    }
-  }, [selectTracks]);
-
+  const currentTracks = tracks.filter(track => selectArray.includes(track._id));
   return (
     <>
       <h2 className={styles.centerblock__h2}>{selectTitles}</h2>
-
       {currentTracks.length > 0 ? (
         currentTracks.map(track => <TrackItem key={track._id} track={track} />)
       ) : (
